a232349c926ec7c425564472256c9f7ff8eab7ec
unknown <uurrnn@gmail.com>
2018-10-10 10:15:52 -0400

    created world generation script

---
+++ Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+
+    const int worldSize = 64;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
